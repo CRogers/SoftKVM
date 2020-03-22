b4c9505f1873123d9f1d2c308e2b9455ae5bb926
Callum Rogers <callumrogers@hotmail.co.uk>
2020-03-22 19:45:11 +0000

    Make a working KVM

--- SoftKVM/Program.cs
+++ SoftKVM/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Management;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
@@ -8,27 +10,39 @@ namespace SoftKVM
 {
     class Program
     {
+        private static readonly String keyboardDeviceId = @"USB\VID_05AC&PID_024F&MI_00\8&1145D480&0&0000";
+        private static readonly Head thisHead = new Head("HDMI (MHL) 1", "HDMI (MHL) 1");
+        private static readonly Head otherHead = new Head("DP", "mDP");
+        private static Head currentHead = thisHead;
+        private static readonly ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Keyboard");
+
         static void Main(string[] args)
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Keyboard");
-            foreach (var managementBaseObject in searcher.Get())
-            {
-                foreach (var propertyData in managementBaseObject.Properties)
-                {
-                    Console.WriteLine("{0}: {1}", propertyData.Name, propertyData.Value);
-                }
-
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine();
-            }
-
             var behaviorSubject = new BehaviorSubject<int>(4);
             var disposable = behaviorSubject
                 .Throttle(TimeSpan.FromMilliseconds(100))
                 .Subscribe(foo =>
                 {
-                    Console.WriteLine("debounced");
+                    if (keyboardExists())
+                    {
+                        if (currentHead == thisHead)
+                        {
+                            return;
+                        }
+                        Console.WriteLine("changing display back to our screen");
+                        thisHead.switchTo();
+                        currentHead = thisHead;
+                    }
+                    else
+                    {
+                        if (currentHead == otherHead)
+                        {
+                            return;
+                        }
+                        Console.WriteLine("changing display back to other screen");
+                        otherHead.switchTo();
+                        currentHead = otherHead;
+                    }
                 });
 
             ManagementEventWatcher watcher = new ManagementEventWatcher();
@@ -41,6 +55,37 @@ namespace SoftKVM
             watcher.Start();
 
             Thread.Sleep((int) Math.Pow(2, 31) - 1);
+        }
+
+        private static bool keyboardExists()
+        {
+            foreach (var managementBaseObject in searcher.Get())
+            {
+                if ((string) managementBaseObject.Properties["DeviceId"].Value == keyboardDeviceId)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+
+    class Head
+    {
+        public Head(string screen1InputName, string screen2InputName)
+        {
+            this.screen1InputName = screen1InputName;
+            this.screen2InputName = screen2InputName;
+        }
+
+        private String screen1InputName { get; }
+        private String screen2InputName { get; }
+
+        public void switchTo()
+        {
+            Process.Start(@"C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe",
+                String.Format("/1:SetActiveInput {0} /2:SetActiveInput {1}", screen1InputName, screen2InputName));
         }
     }
 }
